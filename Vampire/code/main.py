128f55d5e0e92bfa0e4c632a092ad1da8b310ecb
Anmol Raj Shakya <shakyaanmol33@gmail.com>
2025-09-04 16:34:30 +0545

    Kati garo ho bey esko file placement bujhna, create a display surface with a character, also assign the character with movement keys

--- Vampire/code/main.py
+++ Vampire/code/main.py
@@ -1,1 +1,43 @@
 from settings import *
+from player import Player
+
+class Game:
+    #setup
+    def __init__(self):
+        pygame.init()
+        self.display_surf = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT ))
+        pygame.display.set_caption('Vampire_Fighter')
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+        #groups
+        self.all_sprites = pygame.sprite.Group()
+        
+        #sprites
+        self.player = Player((400, 300), self.all_sprites)
+
+    def run (self):
+        while self.running:
+            #dt
+            dt = self.clock.tick() / 1000
+
+            #event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            
+            #update
+            self.all_sprites.update(dt)
+
+
+            #draw
+            self.display_surf.fill('black')
+            self.all_sprites.draw(self.display_surf)
+            pygame.display.update()
+        
+        pygame.quit()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
---
+++ Vampire/code/player.py
@@ -0,0 +1,26 @@
+from settings import *
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(join('images', 'player', 'down', '0.png')).convert_alpha()
+        self.rect = self.image.get_frect(center = pos)
+     
+        #movement
+        self.direction = pygame.Vector2(1,0)
+        self.speed = 500
+       
+    def input(self):
+        keys = pygame.key.get_pressed()
+        self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+        self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+
+
+    def move(self, dt):
+        self.rect.center += self.direction * self.speed * dt
+
+    def update(self, dt):
+        self.input()
+        self.move(dt)
+        
