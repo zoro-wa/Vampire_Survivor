0a4ab7afebe5ceb43ec767483efafca332d161dd
Anmol Raj Shakya <shakyaanmol33@gmail.com>
2025-09-07 19:01:23 +0545

    Improved the collision mechanics

--- Vampire/code/player.py
+++ Vampire/code/player.py
@@ -8,6 +8,7 @@ class Player(pygame.sprite.Sprite):
             join("images", "player", "down", "0.png")
         ).convert_alpha()
         self.rect = self.image.get_frect(center=pos)
+        self.hitbox_rect = self.rect.inflate(-60, 0)
 
         # movement
         self.direction = pygame.Vector2()
@@ -23,24 +24,25 @@ class Player(pygame.sprite.Sprite):
         )
 
     def move(self, dt):
-        self.rect.x += self.direction.x * self.speed * dt
+        self.hitbox_rect.x += self.direction.x * self.speed * dt
         self.collision("horizontal")
-        self.rect.y += self.direction.y * self.speed * dt
+        self.hitbox_rect.y += self.direction.y * self.speed * dt
         self.collision("vertical")
+        self.rect.center = self.hitbox_rect.center
 
     def collision(self, direction):
         for sprite in self.collision_sprites:
-            if sprite.rect.colliderect(self.rect):
+            if sprite.rect.colliderect(self.hitbox_rect):
                 if direction == "horizontal":
                     if self.direction.x > 0:
-                        self.rect.right = sprite.rect.left
+                        self.hitbox_rect.right = sprite.rect.left
                     if self.direction.x < 0:
-                        self.rect.left = sprite.rect.right
+                        self.hitbox_rect.left = sprite.rect.right
                 else:
                     if self.direction.y < 0:
-                            self.rect.top = sprite.rect.bottom
+                            self.hitbox_rect.top = sprite.rect.bottom
                     if self.direction.y > 0:
-                            self.rect.bottom = self.rect.top
+                            self.hitbox_rect.bottom = self.rect.top
 
     def update(self, dt):
         self.input()
